be22eb5ffe3d01b155558e9080063ad0bd795446
Ashish6745 <ashupawar9917@gmail.com>
2025-09-14 14:03:11 +0530

    saved language saved to shared-preference

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mvcapp/localization/translation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+// void main() {
+//   runApp(const MyApp());
+// }
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // initialing sharedpreferences
+  final prefs = await SharedPreferences.getInstance();
+  //  fetching the previously saved language if any
+  final saved = prefs.getString('locale');
+
+  // if any language is saved previously parse it otherwise english by defaul
+  final initialLocale = _localeFromString(saved) ?? const Locale('en', 'US');
+  // Run the app and pass initial locale + prefs instance
+  runApp(MyApp(initialLocale: initialLocale, prefs: prefs));
+}
+
+/// Helper function: Converts 'en_US' string into a Locale object
+Locale? _localeFromString(String? s) {
+  if (s == null) return null; // no saved locale
+  final parts = s.split('_');
+  if (parts.length == 2) return Locale(parts[0], parts[1]); // e.g. 'en_US'
+  return Locale(parts[0]); // e.g. 'en'
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Locale initialLocale;
+  final SharedPreferences prefs;
+  const MyApp({super.key, required this.initialLocale, required this.prefs});
 
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       translations: AppTranslations(),
-      locale: const Locale('en', 'US'),
+      locale: initialLocale,
       fallbackLocale: const Locale('en', 'US'),
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: MyHomePage(title: 'appbar_title'.tr),
+      home: MyHomePage(title: 'appbar_title'.tr,prefs: prefs),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.title,required this.prefs});
   final String title;
-
+  final SharedPreferences prefs;
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _toggleLanguage() {
+  void _toggleLanguage() async {
     print('jkdgdkjgdkghdfgdfg');
     final isEnglish = Get.locale?.languageCode == 'en';
     final newLocale = isEnglish
         ? const Locale('hi', 'IN')
         : const Locale('en', 'US');
     Get.updateLocale(newLocale);
+
+     final localeString = '${newLocale.languageCode}_${newLocale.countryCode ?? ''}'
+        .replaceAll(RegExp(r'_+$'), ''); // cleanup trailing underscores
+    await widget.prefs.setString('locale', localeString);
   }
 
   @override
